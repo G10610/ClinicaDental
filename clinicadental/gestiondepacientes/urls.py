0c4198f6cfe18f70e711793799ed35fafe76aae9
AlejandroE200 <em19009@ues.edu.sv>
2025-06-22 13:44:42 -0600

    Se agrego Editar/Actulizar Pacientes
    Con este cambio el CRUD Pacientes queda completo solo falta dar formato

--- clinicadental/gestiondepacientes/urls.py
+++ clinicadental/gestiondepacientes/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.crudpacientes, name='crudpacientes'),
     path('crearpa/', views.crearpacientessss, name='crearpacientes'),
     path('lista/', views.lista, name='lista'),
-    path('eliminarpa/<int:id>/',
-         views.eliminarpaciente, name='eliminarpaciente'),
+    path('edicionpa/<int:id>', views.edicionPaciente, name='edicionpaciente'),
+    path('editarpa/', views.editarPaciente, name='editarpaciente'),
+    path('eliminarpa/<int:id>/', views.eliminarpaciente, name='eliminarpaciente'),
 
 ]
