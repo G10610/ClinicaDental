69a220ca01b65fbfd64db35284702252b2bd46af
G10610 <cm10151@ues.edu.sv>
2025-10-30 14:29:22 -0600

    Se ha trabajado el CRUD Especialista

--- clinicadental/gestionespecialistas/urls.py
+++ clinicadental/gestionespecialistas/urls.py
@@ -2,18 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('especialistas/', views.crudespecialistas, name='crudespecialistas'),
-    path('crearespecialistas/', views.crearespecialistas,
-         name='crearespecialistas'),
-    path('listaespecialistas/', views.listaespecialistas,
-         name='listaespecialistas'),
 
-    path('edicionespecialista/<int:id>',
-         views.edicionespecialista, name='edicionespecialista'),
+    # copia
+    
 
-    path('editarespecialista/', views.editarespecialista,
-         name='editarespecialista'),
-
-    path('eliminarespecialista/<int:id>/',
-         views.eliminarespecialista, name='eliminarespecialista'),
+    path('listae/', views.listae, name='listae'),
+    path('creare/', views.crear_especialista, name='crearespecialista'),
+    path('edicionespecialista/<int:id>', views.edicionEspecialista, name='edicionespecialista'),
+    
+    # Procesa los datos modificados y guarda los cambios (POST)
+    # Es la acción del formulario dentro de la página de edición.
+    path('editarespecialista/', views.editarEspecialista, name='editarespecialista'),
+    
+    path('eliminar/<int:id>/', views.eliminar_especialista, name='eliminarespecialista'),
+  
 ]
